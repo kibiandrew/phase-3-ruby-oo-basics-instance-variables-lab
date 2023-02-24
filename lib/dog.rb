43e704a79f5db4cf062d3adf8cb6f155728597d6
Andrew Kibiwott <kibiandrew@gmail.com>
2023-02-24 10:56:14 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+    # setter method
+    def name=(dog_name)
+    # assign the @this_dogs_name instance variable a value of whatever is passed as an argument
+    @this_dogs_name = dog_name
+  end
 
+  # getter method
+  def name
+    # access the data from the @this_dogs_name instance variable and return it
+    @this_dogs_name
+  end
 end
